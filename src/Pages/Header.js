710c735b6e6d5e446ad5c7c09e58b457e1dbc2e9
tanvir <tanvirhridoy1011@gmail.com>
2022-10-25 21:42:30 +0600

    navbar set up

--- src/Pages/Header.js
+++ src/Pages/Header.js
@@ -6,16 +6,26 @@ const Header = () => {
     return (
         <Navbar bg="light" expand="lg">
       <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+      <Navbar.Brand href="#home">
+            <img
+              src="https://media.istockphoto.com/photos/organization-process-picture-id1394348763"
+              width="60"
+              height="45"
+              className="d-inline-block align-top"
+              alt=""
+            />
+          </Navbar.Brand>
+        <Navbar.Brand href="#home">TN-Learning</Navbar.Brand>
+        
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Link to="/">Home</Link>
-            <Link to="/courses">Courses</Link>
-            <Link to="/faq">FaQ</Link>
-            <Link to="/blog">Blog</Link>
-            <Link to="/login">Log in</Link>
-            <Link to="/register">Register</Link>
+          <Nav className="mx-auto me-2 text-decoration-none">
+            <Link className=' me-2 text-decoration-none' to="/">Home</Link>
+            <Link className=' me-2 text-decoration-none ' to="/courses">Courses</Link>
+            <Link className=' me-2 text-decoration-none' to="/faq">FaQ</Link>
+            <Link className=' me-2 text-decoration-none' to="/blog">Blog</Link>
+            <Link className=' me-2 text-decoration-none' to="/login">Log in</Link>
+            <Link className=' me-2 text-decoration-none' to="/register">Register</Link>
             
           </Nav>
         </Navbar.Collapse>
