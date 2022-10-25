81cc9678455d6111767b4c215937b983ff030f41
tanvir <tanvirhridoy1011@gmail.com>
2022-10-26 05:52:45 +0600

    data set for details card

--- src/Pages/Course.js
+++ src/Pages/Course.js
@@ -1,12 +1,29 @@
 import React from 'react';
+import { Button, Card } from 'react-bootstrap';
 import { useLoaderData } from 'react-router-dom';
 
 const Course = () => {
     const subject=useLoaderData();
     console.log(subject)
+    const {course_name,img,details,duration,classT}=subject;
     return (
         <div>
-            <h1>This is {subject.course_name} </h1>
+            
+            <Card  >
+            <Card.Title className='mx-5 mt-2 fs-1 fw-bold'>{course_name}</Card.Title>
+      <Card.Img className='px-5' height='350' variant="top" src={img} />
+      <Card.Body className='px-5'>
+        
+        <Card.Text>
+          {details}
+        </Card.Text>
+        <Card.Text>
+          <p>Duration:{duration}</p>
+          <p>Total Class:{classT}</p>
+        </Card.Text>
+        <Button variant="primary">Get Premium Access</Button>
+      </Card.Body>
+    </Card>
         </div>
     );
 };
