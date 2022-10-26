83f09e7ca904e286c74121b7345c55bc5de1f25c
tanvir <tanvirhridoy1011@gmail.com>
2022-10-26 21:50:20 +0600

    set up login and regisration page

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -1,9 +1,37 @@
 import React from 'react';
+import { ButtonGroup } from 'react-bootstrap';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import { Link } from 'react-router-dom';
+import { FaGoogle,FaGithub } from "react-icons/fa";
 
 const Login = () => {
     return (
-        <div>
-            This is login Page
+        <div className='container '>
+            
+            <Form className='w-50 mx-auto' >
+            <ButtonGroup vertical className='text-center' >
+      <Button className='mb-2 mt-2'><FaGoogle/> Log in with Google</Button>
+      <Button className='mb-3' variant="dark"><FaGithub></FaGithub> Log in with GitHub</Button>
+      </ButtonGroup>
+      <h4>You can also Log in with email and password </h4>
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Label>Email address</Form.Label>
+        <Form.Control type="email" placeholder="Enter email" />
+        <Form.Text className="text-muted">
+          We'll never share your email with anyone else.
+        </Form.Text>
+      </Form.Group>
+
+      <Form.Group className="mb-3" controlId="formBasicPassword">
+        <Form.Label>Password</Form.Label>
+        <Form.Control type="password" placeholder="Password" />
+      </Form.Group>
+      <small className='mb-2'>New user, Please <Link to='/register'> Register..</Link>  </small> <br />
+      <Button variant="primary" className='mt-3' type="submit">
+        Submit
+      </Button>
+    </Form>
         </div>
     );
 };
--- src/Pages/Register.js
+++ src/Pages/Register.js
@@ -1,9 +1,40 @@
 import React from 'react';
+import { Button, Form } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+
 
 const Register = () => {
     return (
-        <div>
-            <h2>This is register page</h2>
+        <div className='container '>
+            <Form className='w-50 mx-auto' >
+
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+        <Form.Label>Full Name</Form.Label>
+        <Form.Control name='name' type="text" placeholder="Full Name" />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="formBasicPassword">
+        <Form.Label>Photo url</Form.Label>
+        <Form.Control name='photo' type="text" placeholder="Photo URL" />
+      </Form.Group>
+            
+      
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Label>Email address</Form.Label>
+        <Form.Control name='email' type="email" placeholder="Enter email" required/>
+        <Form.Text className="text-muted">
+          We'll never share your email with anyone else.
+        </Form.Text>
+      </Form.Group>
+
+      <Form.Group className="mb-3" controlId="formBasicPassword">
+        <Form.Label>Password</Form.Label>
+        <Form.Control name='password' type="password" placeholder="Password" required />
+      </Form.Group>
+      <small className='mb-2'>Already have account, Please <Link to='/login'> Log in..</Link>  </small> <br />
+      <Button variant="primary" className='mt-3' type="submit">
+        Submit
+      </Button>
+    </Form>
         </div>
     );
 };
