16286bb79c8e7e16a9d09ff6b4cef88dae1a9afb
tanvir <tanvirhridoy1011@gmail.com>
2022-10-27 22:36:31 +0600

    some style

--- src/Pages/Course.js
+++ src/Pages/Course.js
@@ -1,40 +1,44 @@
 import React from 'react';
-import {  Button, Card } from 'react-bootstrap';
+import { Button, Card } from 'react-bootstrap';
 import { Link, useLoaderData } from 'react-router-dom';
 import Pdf from 'react-to-pdf';
 
-const ref=React.createRef();
+const ref = React.createRef();
 
 const Course = () => {
-    const subject=useLoaderData();
-    console.log(subject)
-    const {course_name,course_id,img,details,duration,classT}=subject;
-    return (
-        <div>
-          
-            
-            <Card className='border border-info rounded' ref={ref} >
-            <Card.Title className='mx-5 mt-2 fs-1 fw-bold'>{course_name}</Card.Title>
-            <Pdf targetRef={ref} filename="code-example.pdf">
-        {({ toPdf }) => <button onClick={toPdf}>Generate Pdf</button>}
-      </Pdf>
-      <Card.Img className='px-5' height='350' variant="top" src={img} />
-      <Card.Body className='px-5'>
-        
-        <Card.Text>
-          {details}
-        </Card.Text>
-        <Card.Text>
-          <p>Duration:{duration}</p>
-          <p>Total Class:{classT}</p>
-        </Card.Text>
-        <Link to={`/courses/1/date${course_id}`}
-        ><Button variant="outline-info" className='w-100  '>Get Premium Access</Button></Link>
-        
-      </Card.Body>
-    </Card>
+  const subject = useLoaderData();
+  console.log(subject)
+  const { course_name, course_id, img, details, duration, classT } = subject;
+  return (
+    <div>
+
+
+
+      <Card className='border border-info rounded' ref={ref} >
+        <div className='d-flex justify-content-between'>
+          <Card.Title className='mx-5 mt-2 fs-1 fw-bold'>{course_name}</Card.Title>
+          <Pdf targetRef={ref} filename="code-example.pdf">
+            {({ toPdf }) => <Button variant="primary" className='me-5 size="sm"' onClick={toPdf}>Generate Pdf</Button>}
+          </Pdf>
         </div>
-    );
+
+        <Card.Img className='px-5' height='350' variant="top" src={img} />
+        <Card.Body className='px-5'>
+
+          <Card.Text>
+            {details}
+          </Card.Text>
+          <Card.Text>
+            <p>Duration:{duration}</p>
+            <p>Total Class:{classT}</p>
+          </Card.Text>
+          <Link to={`/courses/1/date${course_id}`}
+          ><Button variant="outline-info" className='w-100  '>Get Premium Access</Button></Link>
+
+        </Card.Body>
+      </Card>
+    </div>
+  );
 };
 
 export default Course;
